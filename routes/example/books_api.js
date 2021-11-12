e7d2d7caf4d8e39390e9bbc015dca7462b4a23f8
Viody Alfaridzi <viody75@gmail.com>
2021-11-12 16:51:28 +0800

    add git

---
+++ routes/example/books_api.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+const authUser = require("../../middlewares/auth_users");
+var booksApiController = require('../../controllers/books_api_controller');
+
+// get books data
+router.get('/getbooks', booksApiController.getbooks);
+
+router.get('/getbooks2', authUser, booksApiController.getbooks);
+
+// add a new book
+router.post('/addbook', booksApiController.addbook);
+
+// update a book
+router.put('/updatebook', booksApiController.updatebook);
+
+// delete a book
+router.post('/deletebook', booksApiController.deletebook);
+
+module.exports = router;
---
+++ routes/example/inventories_api.js
@@ -0,0 +1,15 @@
+const express = require('express');
+var router = express.Router();
+
+const authUser = require('../../middlewares/auth_users');
+var invApiController = require('../../controllers/inv_api_controller');
+
+router.get('/getinventories', invApiController.getInventories);
+
+router.get('/getinputedinventories', authUser, invApiController.getInputedInv);
+
+router.post('/addinventory', authUser, invApiController.addItemInv);
+
+router.post('/updateinventory', authUser, invApiController.updateItemInv);
+
+module.exports = router
